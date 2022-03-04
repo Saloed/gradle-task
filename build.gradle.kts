8f66641632625c074b22751557eb68860c651d98
Gloff228 <romo4ka101@gmail.com>
2022-03-04 22:01:01 +0300

    complete

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,11 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
     implementation("com.google.guava:guava:31.0.1-jre")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }

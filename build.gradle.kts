b759d742af95f48bcb15bcea726f981f2cd97baf
Roman S <ua.romansyslikov@gmail.com>
2022-03-05 23:06:05 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ repositories {
 
 dependencies {
     implementation("com.google.guava:guava:31.0.1-jre")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")

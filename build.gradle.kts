0ed36713be960da7b1d2ea95e9c738c916bf16c7
AndrewRedFox <90340969+AndrewRedFox@users.noreply.github.com>
2022-03-05 23:06:07 +0300

    q

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,13 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
-
-    implementation("com.google.guava:guava:31.0.1-jre")
-
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
+
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation ("com.google.guava:guava:31.0.1-jre")
+
 }
 
 tasks.test {

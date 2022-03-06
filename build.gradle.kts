ec4faf31734144a4a9998f31ddfc4041c1c354d3
Qustarnick <qustarnick@gmail.com>
2022-03-06 19:40:23 +0300

    done!

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("com.google.guava:guava:31.0.1-jre")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

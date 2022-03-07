c22a00e51839f6284cfd29e2eabce946ce2b0748
Maksimeg <egorov.mo@edu.spbstu.ru>
2022-03-07 12:57:02 +0300

    q

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,8 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
-
     implementation("com.google.guava:guava:31.0.1-jre")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 }

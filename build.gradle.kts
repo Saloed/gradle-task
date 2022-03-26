868c51ddc53f54d4315ac403f25e822c020632b7
Дмитрий <dimonussir5@gmail.com>
2022-03-27 00:26:30 +0300

    Тучков Дмитрий Группа 3530901/10005

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("com.google.guava:guava:31.0.1-jre")
+
+    testImplementation(kotlin("test-junit5"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

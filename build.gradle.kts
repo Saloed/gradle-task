c8931e5b5effca7809b036866cd59f56c3a6f820
Flimill <uhovad03@gmail.com>
2022-06-14 19:06:02 +0300

    push

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+}
+
+group = "ru.example"
+version = "1.01"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
+    implementation("com.google.guava:guava:31.1-jre")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
